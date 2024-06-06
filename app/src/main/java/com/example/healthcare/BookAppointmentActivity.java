201d4b53d437d7aaa233dc7b6511f8c36c6d1c07
ikaay1 <imgbemele@gmail.com>
2024-06-06 16:38:18 -0500

    implement date and time pickers

--- app/src/main/java/com/example/healthcare/BookAppointmentActivity.java
+++ app/src/main/java/com/example/healthcare/BookAppointmentActivity.java
@@ -1,13 +1,17 @@
 package com.example.healthcare;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +19,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class BookAppointmentActivity extends AppCompatActivity {
 
@@ -63,6 +70,76 @@ public class BookAppointmentActivity extends AppCompatActivity {
             }
         });
 
+
+
+        final Calendar c = Calendar.getInstance();
+        final int[] mYear = {c.get(Calendar.YEAR)};
+        final int[] mMonth = {c.get(Calendar.MONTH)};
+        final int[] mDay = {c.get(Calendar.DAY_OF_MONTH) + 1};
+
+        EditText editTextDate = findViewById(R.id.editTextDate);
+        EditText editTextTime = findViewById(R.id.editTextTime);
+
+        editTextDate.setKeyListener(null);
+        editTextTime.setKeyListener(null);
+
+        editTextDate.setText((mDay[0]) + "/" + (mMonth[0] + 1) + "/" + mYear[0]);
+
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(BookAppointmentActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                editTextDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                mDay[0] = dayOfMonth;
+                                mMonth[0] = monthOfYear;
+                                mYear[0] = year;
+
+                            }
+                        }, mYear[0], mMonth[0], mDay[0]);
+                datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+                datePickerDialog.show();
+            }
+        });
+
+
+        final int[] mHour = {c.get(Calendar.HOUR_OF_DAY)};
+        final int[] mMinute = {c.get(Calendar.MINUTE)};
+
+        Date date = new Date();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+
+        editTextTime.setText((String.valueOf(mHour[0]).length() == 1 ? "0" + mHour[0] : mHour[0]) + ":" + (String.valueOf(mMinute[0]).length() == 1 ? "0" + mMinute[0] : mMinute[0]));
+
+        editTextTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(BookAppointmentActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                editTextTime.setText((String.valueOf(hourOfDay).length() == 1 ? "0" + hourOfDay : hourOfDay) + ":" + (String.valueOf(minute).length() == 1 ? "0" + minute : minute));
+                                mHour[0] = hourOfDay;
+                                mMinute[0] = minute;
+                            }
+                        }, mHour[0], mMinute[0], false);
+                timePickerDialog.show();
+            }
+        });
+
         Button btnBookAppointment = findViewById(R.id.buttonAppBook);
         btnBookAppointment.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +147,8 @@ public class BookAppointmentActivity extends AppCompatActivity {
                 Database db = new Database(getApplicationContext(), "healthcare", null, 1);
                 SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
                 String username = sharedPreferences.getString("username", "");
-                String date = ((EditText) findViewById(R.id.editTextDate)).getText().toString();
-                String time = ((EditText) findViewById(R.id.editTextTime)).getText().toString();
+                String date = editTextDate.getText().toString();
+                String time = editTextTime.getText().toString();
                 if (db.checkAppointmentExists(username, title + " => " + name, address, mobile, date, time)){
                     Toast.makeText(getApplicationContext(), "Appointment already booked", Toast.LENGTH_SHORT).show();
                 } else {
--- app/src/main/java/com/example/healthcare/CartLabActivity.java
+++ app/src/main/java/com/example/healthcare/CartLabActivity.java
@@ -1,5 +1,7 @@
 package com.example.healthcare;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,10 +9,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +24,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 public class CartLabActivity extends AppCompatActivity {
@@ -50,7 +56,7 @@ public class CartLabActivity extends AppCompatActivity {
         }
 
         SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-        String username = sharedPreferences.getString("username", "").toString();
+        String username = sharedPreferences.getString("username", "");
 
         Database db = new Database(getApplicationContext(), "healthcare", null, 1);
         ArrayList arr = db.getCartData(username, text);
@@ -94,6 +100,70 @@ public class CartLabActivity extends AppCompatActivity {
         Button btnCheckout = findViewById(R.id.buttonCLCheckout);
         float finalTotalAmount = totalAmount;
         EditText editTextDate = findViewById(R.id.editTextCLDate);
+
+        editTextDate.setKeyListener(null);
+        editTextTime.setKeyListener(null);
+
+        final Calendar c = Calendar.getInstance();
+        final int[] mYear = {c.get(Calendar.YEAR)};
+        final int[] mMonth = {c.get(Calendar.MONTH)};
+        final int[] mDay = {c.get(Calendar.DAY_OF_MONTH) + 1};
+
+        editTextDate.setText((mDay[0]) + "/" + (mMonth[0] + 1) + "/" + mYear[0]);
+
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CartLabActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                editTextDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                mDay[0] = dayOfMonth;
+                                mMonth[0] = monthOfYear;
+                                mYear[0] = year;
+                            }
+                        }, mYear[0], mMonth[0], mDay[0]);
+                datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+                datePickerDialog.show();
+            }
+        });
+
+
+        final int[] mHour = {c.get(Calendar.HOUR_OF_DAY)};
+        final int[] mMinute = {c.get(Calendar.MINUTE)};
+
+        Date date = new Date();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+
+        editTextTime.setText((String.valueOf(mHour[0]).length() == 1 ? "0" + mHour[0] : mHour[0]) + ":" + (String.valueOf(mMinute[0]).length() == 1 ? "0" + mMinute[0] : mMinute[0]));
+
+        editTextTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(CartLabActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                editTextTime.setText((String.valueOf(hourOfDay).length() == 1 ? "0" + hourOfDay : hourOfDay) + ":" + (String.valueOf(minute).length() == 1 ? "0" + minute : minute));
+                                mHour[0] = hourOfDay;
+                                mMinute[0] = minute;
+                            }
+                        }, mHour[0], mMinute[0], false);
+                timePickerDialog.show();
+            }
+        });
 
         btnCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
